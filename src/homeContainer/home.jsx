4f1187d0628ace3487f0b1bdc34cd291466f4024
khodges <zkellyhodges@gmail.com>
2022-04-25 08:43:13 -0700

    changed homepage based on logged in or not

--- src/homeContainer/home.jsx
+++ src/homeContainer/home.jsx
@@ -2,17 +2,33 @@ import '../App.css'
 import { Link } from 'react-router-dom'
 
 const Home = () =>{
-    return(
-    <div id="home">
-        <div id="home-taglines">
-            <h1>Pour Pair</h1>
-            <h2>Find & save wines to your <Link to="/saved-wines">wine cellar</Link></h2>
-            <h2>Look up wines to <Link to="/pair">pair</Link> with your meals <br></br>or meal suggestions for your favorite wines</h2>
-        </div>
-        
-        <img id="home-img"src="https://media.giphy.com/media/JsbqCNmqYGZ62t8mPJ/giphy-downsized-large.gif"></img>
-    </div>
-    )  
+    const user = JSON.parse(localStorage.getItem('props.currentUser'))
+    if(user !== null){
+        return(
+            <div id="home">
+                <div id="home-taglines">
+                    <h1>Pour Pair</h1>
+                    <h2>Find & save wines to your <Link to="/saved-wines">wine cellar</Link></h2>
+                    <h2>Look up wines to <Link to="/pair">pair</Link> with your meals <br></br>or meal suggestions for your favorite wines</h2>
+                </div>
+                
+                <img id="home-img"src="https://media.giphy.com/media/JsbqCNmqYGZ62t8mPJ/giphy-downsized-large.gif"></img>
+            </div>
+            )  
+    }else{
+        return(
+            <div id="home">
+                <div id="home-taglines">
+                    <h1>Pour Pair</h1>
+                    <h2>Find & save wines to your  digital wine cellar</h2>
+                    <h2>Look up wines to <Link to="/pair">pair</Link> with your meals <br></br>or meal suggestions for your favorite wines</h2>
+                </div>
+                
+                <img id="home-img"src="https://media.giphy.com/media/JsbqCNmqYGZ62t8mPJ/giphy-downsized-large.gif"></img>
+            </div>
+            )  
+    }
+   
 }
   
 
